726558aea51aa3ec2edb69fd48e69a4866ef776d
karunkumar2398-blip <karunkumar2398@gmail.com>
2026-09-21 15:37:58 +0530

    Add headless mode and performance options to driver
    Updated Chrome driver options for headless mode and performance.

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -4,7 +4,12 @@ from selenium.webdriver.chrome.options import Options
 
 def create_driver():
     options = Options()
-    options.add_argument("--start-maximized")
+
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--window-size=1920,1080")
 
     driver = webdriver.Chrome(options=options)
 
